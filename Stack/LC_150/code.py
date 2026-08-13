ed54d138372a9c5e9e7d89043bbaa2411ea028fe
Amol Dadarao Rathod <amolrathod7875470402@gmail.com>
2026-08-13 14:01:10 +0000

    LC_150

---
+++ Stack/LC_150/code.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token in "+-*/":
+                right = stack.pop()
+                left = stack.pop()
+                if token == "+":
+                    result = left + right
+                elif token == "-":
+                    result = left - right
+                elif token == "*":
+                    result = left * right
+                else:
+                    result = int(left / right)
+                stack.append(result)
+            else:
+                stack.append(int(token))
+        return stack[-1]
