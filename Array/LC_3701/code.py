907e0f1e835a7d94e5c624c9a3458d9889cd3c76
amolrathod7875 <amolrathod7875470402@gmail.com>
2026-08-20 14:01:37 +0530

    GFG_Heap_Sort

--- Array/LC_3701/code.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def alternatingSum(self, nums: list[int]) -> int:
-        total = 0
-        for i, num in enumerate(nums):
-            if i % 2 == 0:
-                total += num
-            else:
-                total -= num
-        return total
--- Hash_Map/LC_349/code.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        set1 = set(nums1)
-        set2 = set(nums2)
-        return list(set1 & set2)
